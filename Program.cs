647359bc5981efdb694a18a637ecd063c54a71a8
Leology0 <leology42@gmail.com>
2023-01-11 10:00:33 -0700

    change

--- Program.cs
+++ Program.cs
@@ -8,40 +8,41 @@ Console.WriteLine("WELCOME TO THE POKEMON QUIZ");
 //Variables
 int score = 0;
 
-
 // Inputs
 // Question 1
 Console.WriteLine("1.What is the gen 1 fire type starter");
 Console.Write("Q1 Answer:");
 string answer1 = Console.ReadLine().ToLower();
-
+// Check answer 1
+if (answer1 == "charmander") {
+    score++;
+    Console.WriteLine("Correct");
+}
 // Question 2
 Console.WriteLine("\n2.How many Pokemon are in the Pokemon Scarlet and Violet Pokedex");
 Console.Write("Q2 Answer:");
 string answer2 = Console.ReadLine().ToLower();
+if (answer2 == "400") {
+    score++;
+    Console.WriteLine("Correct");
+}
 
 // Question 3
 Console.WriteLine("\n3.In what games were mega evolution for Pokemon introduced");
 Console.Write("Q3 Answer:");
 string answer3 = Console.ReadLine().ToLower();
+if (answer3 == "pokemon x and y") {
+    score++;
+    Console.WriteLine("Correct");
+}
 
 // Question 4
 Console.WriteLine("\n4.What Pokemon was Ash Ketchum's partner in the popular Pokemon show");
 Console.Write("Q4 Answer:");
 string answer4 = Console.ReadLine().ToLower();
-
-// Check Answers
-if (answer1 == "charmander") {
-    score++;
-} 
-if (answer2 == "400") {
-    score++;
-}
-if (answer3 == "pokemon x and y") {
-    score++;
-}
 if (answer4 == "pikachu") {
     score++;
+    Console.WriteLine("Correct");
 }
 
 // Evaluate Score
